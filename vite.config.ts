30f16efacae2d29011f274018a64b8f6adcaeb33
Lewin Kelly <heli@odex.cf>
2026-08-16 19:43:28 -0400

    Improved SvelteKit configuration

--- vite.config.ts
+++ vite.config.ts
@@ -13,12 +13,21 @@ export default defineConfig({
 					filename.split(/[/\\]/).includes("node_modules")
 						? undefined
 						: true,
+
+				experimental: {
+					async: true,
+				},
 			},
 
 			// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 			// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 			// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 			adapter: adapter(),
+
+			experimental: {
+				handleRenderingErrors: true,
+				remoteFunctions: true,
+			},
 		}),
 	],
 })
