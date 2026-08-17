506117006605ea06783b14b9696e99baaf06e162
Lewin Kelly <heli@odex.cf>
2026-08-17 17:03:48 -0400

    Improved example script

--- example/index.js
+++ example/index.js
@@ -4,12 +4,24 @@ new Dom(
 	[tag("title").addChildren(text("JS tools test"))],
 	[
 		tag("h1").addChildren(text("JS tools test")),
-		tag("p").addChildren(text("hello, world")),
+		tag("p")
+			.addAttribute("style", "color: blue")
+			.addChildren(text("hello, world")),
 
 		tag("button")
 			.addEvent("click", () => {
 				console.log("sup")
 			})
 			.addChildren(text("Click me")),
+
+		tag("div").addChildren(
+			tag("h2").addChildren(text("List of things I can do")),
+			tag("ul").addChildren(
+				tag("li").addChildren(text("write code")),
+				tag("li").addChildren(text("trial & error")),
+				tag("li").addChildren(text("reinvent the wheel")),
+				tag("li").addChildren(text("procrastinate"))
+			)
+		),
 	]
 ).render()
