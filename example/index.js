35d7bc661a3dc771cf99cca57389f7ad4e24c8ed
Lewin Kelly <heli@odex.cf>
2026-08-17 15:34:22 -0400

    Dom class and .render() function

--- example/index.js
+++ example/index.js
@@ -1,3 +1,31 @@
 console.log("hello world")
 
+const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 
+function randomStringID() {
+	const result = []
+
+	for (let i = 0; i < 9; i++)
+		result.push(chars[Math.floor(Math.random() * chars.length)])
+
+	return result.join("")
+}
+
+class Dom {
+	constructor(head, body) {
+		this.head = head
+		this.body = body
+	}
+
+	render() {
+		for (const element of this.head) {
+			const node = renderElement(element)
+			document.head.appendChild(node)
+		}
+
+		for (const element of this.body) {
+			const node = renderElement(element)
+			document.body.appendChild(node)
+		}
+	}
+}
