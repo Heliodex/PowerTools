c333eee31c3f6f3ce2fa647562434f26c6414333
Lewin Kelly <heli@odex.cf>
2026-08-17 14:08:28 -0400

    Authentication, homepage, and logout implementations

---
+++ src/routes/(main)/api.remote.ts
@@ -0,0 +1,20 @@
+// Contains various api methods that cannot be accessed in a page context, usually because they are requested from a component.
+
+import { redirect } from "@sveltejs/kit"
+import { form, getRequestEvent } from "$app/server"
+import { authorise, cookieName, invalidateSession } from "$lib/server/auth"
+
+export const logout = form(async () => {
+	const { cookies, locals } = getRequestEvent()
+	const { session } = await authorise(locals)
+
+	await invalidateSession(session)
+	cookies.delete(cookieName, { path: "/" })
+
+	redirect(302, "/")
+})
+
+export const statusping = form(() => {
+	// does nothing
+	// hooks.server.ts will update the user's status when pinged
+})
---
+++ src/routes/(main)/home/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { logout } from "../api.remote"
+</script>
+
+<h1>You are logged in</h1>
+
+<form {...logout} class="pt-4">
+	<button
+		class="btn text-white bg-red-500 hover:bg-red-600 active:bg-red-400 px-3 py-1.5 rounded-lg">
+		<b>Log out</b>
+	</button>
+</form>
