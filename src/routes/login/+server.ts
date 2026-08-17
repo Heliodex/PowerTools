442e7123bad610a62f1c375dbce940bb5b207136
Lewin Kelly <heli@odex.cf>
2026-08-17 14:22:20 -0400

    Fix GET function params

--- src/routes/login/+server.ts
+++ src/routes/login/+server.ts
@@ -3,12 +3,12 @@ import { redirect } from "@sveltejs/kit"
 import { dev } from "$app/environment"
 import { getHackClubAuthUrl } from "$lib/server/auth"
 
-export async function GET(event: RequestEvent) {
+export async function GET({ cookies }: RequestEvent) {
 	// Generate a random state for CSRF protection
 	const state = crypto.randomUUID()
 
 	// Store state in a cookie for verification in the callback
-	event.cookies.set("hca_state", state, {
+	cookies.set("hca_state", state, {
 		path: "/",
 		httpOnly: true,
 		maxAge: 60 * 10, // 10 minutes
