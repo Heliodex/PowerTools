d99d48146aace16ff0660344b03a0402d9034b1b
Lewin Kelly <heli@odex.cf>
2026-08-18 11:40:54 -0400

    Add accordion components from OpenBlox

---
+++ src/lib/components/AccordionItem.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import type { Snippet } from "svelte"
+
+	const {
+		title,
+		children
+	}: {
+		title: string
+		children: Snippet
+	} = $props()
+</script>
+
+<details name="details" class="bg-neutral-700 transition-colors rounded-lg">
+	<summary class="p-2 px-4 block cursor-pointer">
+		{title}
+	</summary>
+	<p class="px-4 py-2 bg-neutral-800 rounded-b-lg">
+		{@render children()}
+	</p>
+</details>
