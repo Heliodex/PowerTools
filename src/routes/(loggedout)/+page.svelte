0fc853ddd6deefb3c0a736fef01b6cfaaefc56b1
Lewin Kelly <heli@odex.cf>
2026-08-18 09:01:29 -0400

    Add Hack Club logo in top left corner of landing page

--- src/routes/(loggedout)/+page.svelte
+++ src/routes/(loggedout)/+page.svelte
@@ -1,2 +1,30 @@
+<a href="https://hackclub.com/">
+	<img
+		id="hc"
+		class="absolute top-0 left-8 border-0 w-32 z-999"
+		src="https://assets.hackclub.com/flag-orpheus-top.svg"
+		alt="Hack Club" />
+</a>
+
+
 <h1>Welcome to YSWS</h1>
 <p>Why not <a href="/login">log in</a>?</p>
+
+<style>
+	#hc:hover {
+		animation: rotate 0.75s ease-in-out;
+		animation-iteration-count: 2;
+	}
+
+	@keyframes rotate {
+		from {
+			transform: rotate(0deg);
+		}
+		50% {
+			transform: rotate(-3deg) translate(2px, -3px);
+		}
+		to {
+			transform: rotate(0deg);
+		}
+	}
+</style>
