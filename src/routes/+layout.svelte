8aecd468a9bc5d266e57c4a210fd7504ce7b2b44
Lewin Kelly <heli@odex.cf>
2026-08-19 18:55:38 -0400

    Add analytics with Plausible

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,6 +11,10 @@
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
+
+	<!-- Privacy-friendly analytics by Plausible -->
+	<script async src="https://plausible.io/js/pa-twHcZYCXszjlsDQhYOAJl.js"></script>
+	<script>window.plausible=window.plausible||function(){(plausible.q=plausible.q||[]).push(arguments)},plausible.init=plausible.init||function(i){plausible.o=i||{}};plausible.init()</script>
 </svelte:head>
 
 <header class="max-w-280 mx-auto flex">
