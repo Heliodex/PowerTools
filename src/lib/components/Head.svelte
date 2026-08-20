dcaf0a9a9c7bcfedd2354559996b63da96949ee4
Lewin Kelly <heli@odex.cf>
2026-08-20 14:19:44 -0400

    Add Head.svelte component
    forgot to commit untracked whoops

---
+++ src/lib/components/Head.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+import { programmeName } from "#lib/assets/config.js"
+
+let {
+	title,
+	description = "",
+	ogImage = "",
+}: {
+	title?: string
+	description?: string
+	ogImage?: string
+} = $props()
+</script>
+
+<svelte:head>
+	{#if title}
+		<title>{title} - {programmeName}</title>
+	{:else}
+		<title>{programmeName}</title>
+	{/if}
+
+	{#if description}
+		<meta name="description" content={description}>
+	{/if}
+	{#if ogImage}
+		<meta property="og:image" content={ogImage}>
+	{/if}
+</svelte:head>
